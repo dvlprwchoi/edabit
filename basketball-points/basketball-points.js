dc0bd50a5afed37326123171e6d6543de37a0431
dvlprwchoi <dvlpr.wchoi@gmail.com>
2022-04-16 21:40:54 -0500

    Add basketball points

---
+++ basketball-points/basketball-points.js
@@ -0,0 +1,7 @@
+function points(twoPointers, threePointers) {
+  return twoPointers * 2 + threePointers * 3;
+}
+
+// const points = (twos, threes) => 2 * twos + 3 * threes;
+
+console.log(points(38, 8));
