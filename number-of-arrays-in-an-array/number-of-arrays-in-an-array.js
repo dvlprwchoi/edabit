e7a75e62b4c40b5ed74d81d1320da4b13e3abbb9
dvlprwchoi <dvlpr.wchoi@gmail.com>
2022-04-20 17:24:30 -0500

    Add buggy code part5

--- number-of-arrays-in-an-array/number-of-arrays-in-an-array.js
+++ number-of-arrays-in-an-array/number-of-arrays-in-an-array.js
@@ -1,10 +1,18 @@
+// function numOfSubbarrays(arr) {
+//   for (let i = 0; i < arr.length; i++) {
+//     if (!Array.isArray(arr[i])) {
+//       return 0;
+//     }
+//     return arr.length;
+//   }
+// }
+
+// function numOfSubbarrays(arr) {
+//   return Array.isArray(arr[0]) ? arr.length : 0;
+// }
+
 function numOfSubbarrays(arr) {
-  for (let i = 0; i < arr.length; i++) {
-    if (!Array.isArray(arr[i])) {
-      return 0;
-    }
-    return arr.length;
-  }
+  return typeof arr[0] == 'number' ? 0 : arr.length;
 }
 
 console.log(
