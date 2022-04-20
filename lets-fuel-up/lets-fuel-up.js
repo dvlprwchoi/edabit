0ec0a50302c383af3ee302eac3dd360de7b4102b
dvlprwchoi <dvlpr.wchoi@gmail.com>
2022-04-20 09:43:22 -0500

    Add let's fuel up

---
+++ lets-fuel-up/lets-fuel-up.js
@@ -0,0 +1,10 @@
+// function calculateFuel(n) {
+//   if (n < 10) {
+//     return 100;
+//   }
+//   return n * 10;
+// }
+
+calculateFuel = (n) => (n < 10 ? 100 : n * 10);
+
+console.log(calculateFuel(9));
