a9c5c7f860f5b963e65c1cbad92466a5931e486a
dvlprwchoi <dvlpr.wchoi@gmail.com>
2022-04-20 09:50:44 -0500

    Add is the number less than or equal to zero

---
+++ is-the-number-less-than-or-equal-to-zero/is-the-number-less-than-or-equal-to-zero.js
@@ -0,0 +1,10 @@
+// function lessThanOrEqualToZero(num) {
+//   if (num <= 0) {
+//     return true;
+//   }
+//   return false;
+// }
+
+const lessThanOrEqualToZero = (num) => (num <= 0 ? true : false);
+
+console.log(lessThanOrEqualToZero(-2));
