08f5256fdaea170250b621a69670c326fdafde2c
dvlprwchoi <dvlpr.wchoi@gmail.com>
2022-04-20 10:00:14 -0500

    Add pair management

--- pair-management/pair-management.js
+++ pair-management/pair-management.js
@@ -1,5 +1,8 @@
-function makePair(num1, num2) {
-  return [num1, num2];
-}
+// function makePair(num1, num2) {
+//   return [num1, num2];
+// }
 
+function makePair(num1, num2) {
+  return [...arguments];
+}
 console.log(makePair(51, 21));
