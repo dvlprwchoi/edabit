c495655be4d48cb1e44518aebad91d97ba3fb9a5
dvlprwchoi <dvlpr.wchoi@gmail.com>
2022-04-20 09:15:22 -0500

    Add using the and operator

---
+++ using-the-and-operator/using-the-and-operator.js
@@ -0,0 +1,11 @@
+// function and(a, b) {
+//   if (a == true && b == true) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+
+const and = (a, b) => (a == true && b == true ? true : false);
+
+console.log(and(false, true));
