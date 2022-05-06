18a44dad8f22f91d22d0086fdca46f4cb6778472
dvlprwchoi <dvlpr.wchoi@gmail.com>
2022-05-06 10:35:17 -0500

    Add string and number conversions

---
+++ string-and-number-conversions/string-and-number-conversions.js
@@ -0,0 +1,15 @@
+// function intToString(num) {
+//   return String(num);
+// }
+
+// function stringToInt(num) {
+//   return Number(num);
+// }
+
+const [intToString, stringToInt] = [String, Number];
+
+// const intToString = (n) => '' + n;
+// const stringToInt = (n) => +n;
+
+console.log(intToString(29348));
+console.log(stringToInt('29348'));
