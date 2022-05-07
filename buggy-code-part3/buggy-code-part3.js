1552fb555321dcc461f328f58658d82aa42a4e52
dvlprwchoi <dvlpr.wchoi@gmail.com>
2022-05-07 15:54:13 -0500

    update buggy code part3

--- buggy-code-part3/buggy-code-part3.js
+++ buggy-code-part3/buggy-code-part3.js
@@ -1,9 +1,11 @@
-function sumArray(arr) {
-  let sum = 0;
-  for (let i = 0; i < arr.length; i++) {
-    sum += arr[i];
-  }
-  return sum;
-}
+// function sumArray(arr) {
+//   let sum = 0;
+//   for (let i = 0; i < arr.length; i++) {
+//     sum += arr[i];
+//   }
+//   return sum;
+// }
+
+let sumArray = (a) => a.reduce((a, b) => a + b);
 
 console.log(sumArray([0, 4, 8, 12]));
