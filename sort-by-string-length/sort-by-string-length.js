bca7a6114b8163567d8bc378c88bd28d1cf881a5
dvlprwchoi <dvlpr.wchoi@gmail.com>
2022-05-16 14:01:42 -0500

    Add sort by string length

---
+++ sort-by-string-length/sort-by-string-length.js
@@ -0,0 +1,19 @@
+// function sortByLength(arr) {
+//   const mapped = data.map((v, i) => {
+//     return { i, value: someSlowOperation(v) };
+//   });
+
+//   mapped.sort((a, b) => {
+//     if (a.value > b.value) {
+//       return 1;
+//     } else if (a.value < b.value) {
+//       return -1;
+//     }
+//     return 0;
+//   });
+//   const result = mapped.map((v) => data[v.i]);
+// }
+
+function sortByLength(arr) {
+  return arr.sort((a, b) => a.length - b.length);
+}
