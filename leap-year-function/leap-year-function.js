694f5c85c3a2b7deebe70ec13df416a2c05aaa4d
dvlprwchoi <dvlpr.wchoi@gmail.com>
2022-05-27 16:11:45 -0500

    Add leap year function

---
+++ leap-year-function/leap-year-function.js
@@ -0,0 +1,12 @@
+function leapYear(year) {
+  if (year % 100 === 0) {
+    if (year % 400 === 0) {
+      return true;
+    } else {
+      return false;
+    }
+  } else if (year % 4 === 0) {
+    return true;
+  }
+  return false;
+}
