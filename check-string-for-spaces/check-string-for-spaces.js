92387eeaba7097788df208108ef228a3ee877042
dvlprwchoi <dvlpr.wchoi@gmail.com>
2022-06-02 15:13:34 -0500

    Add check string for spaces

---
+++ check-string-for-spaces/check-string-for-spaces.js
@@ -0,0 +1,11 @@
+// function hasSpaces(str) {
+//   return str.split('').includes(' ');
+// }
+
+function hasSpaces(str) {
+  return str.indexOf(' ') != -1;
+}
+
+function hasSpaces(str) {
+  return str.includes(' ');
+}
