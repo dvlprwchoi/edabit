2e059232f75169638b438321c5ec41b403ec2b20
dvlprwchoi <dvlpr.wchoi@gmail.com>
2022-06-16 09:45:31 -0500

    Add es6 destructuring arrays 4

---
+++ es6-destructuring-arrays-4/es6-destructuring-arrays-4.js
@@ -0,0 +1,7 @@
+// Change the string so that it will evaluate to head = 1 and tail = [2, 3, 4]
+// Use the Rest element
+// const str = '[head, tail] = [1, 2, 3, 4]';
+
+const str = ([head, ...tail] = [1, 2, 3, 4]);
+
+console.log(tail);
