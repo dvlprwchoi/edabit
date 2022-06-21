1b391a85ec1a9e4ee7731431db652f9ce5d72b77
dvlprwchoi <dvlpr.wchoi@gmail.com>
2022-06-21 10:09:27 -0500

    Update

--- es6-destructuring-arrays-3/es6-destructuring-arrays-3.js
+++ es6-destructuring-arrays-3/es6-destructuring-arrays-3.js
@@ -2,5 +2,8 @@
 // DO NOT USE var [lips] = arr[2]
 // "eyes", "nose", and "ears" should not be assigned to anything
 
+// const arr = ['eyes', 'nose', 'lips', 'ears'];
+// let [eyes, nose, lips, ears] = arr;
+
 const arr = ['eyes', 'nose', 'lips', 'ears'];
-let [eyes, nose, lips, ears] = arr;
+let [, , lips] = arr;
