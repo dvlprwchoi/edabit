77f43f3df2a21cbaa7028f1dffc7a9fb5e2c6148
dvlprwchoi <dvlpr.wchoi@gmail.com>
2022-07-01 12:07:54 -0500

    Update

--- concatenating-two-integer-arrays/concatenating-two-integer-arrays.js
+++ concatenating-two-integer-arrays/concatenating-two-integer-arrays.js
@@ -1,3 +1,14 @@
 function concat(arr1, arr2) {
   return arr1.concat(arr2);
 }
+
+function concat(arr1, arr2) {
+  return [...arr1, ...arr2];
+}
+
+function concat(arr1, arr2) {
+  for (item of arr2) {
+    arr1.push(item);
+  }
+  return arr1;
+}
