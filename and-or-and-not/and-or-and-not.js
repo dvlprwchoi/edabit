7b67e53736f2115420d160af9a54da917d7c5b55
dvlprwchoi <dvlpr.wchoi@gmail.com>
2022-07-27 07:55:34 -0500

    Add and or and not

---
+++ and-or-and-not/and-or-and-not.js
@@ -0,0 +1,15 @@
+function NOT(n) {
+  return !n ? 1 : 0;
+}
+
+function NOT(n) {
+  return n === 0 ? 1 : 0;
+}
+
+function AND(a, b) {
+  return a === 1 && b === 1 ? 1 : 0;
+}
+
+function OR(a, b) {
+  return a === 1 || b === 1 ? 1 : 0;
+}
