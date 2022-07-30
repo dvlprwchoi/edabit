78a6b9504912dc2b35e95fd8b14a098df7fb376a
dvlprwchoi <dvlpr.wchoi@gmail.com>
2022-07-30 14:40:24 -0500

    Add string to integer and vice versa

---
+++ string-to-integer-and-vice-versa/string-to-integer-and-vice-versa.js
@@ -0,0 +1,26 @@
+function toInt(str) {
+  return parseInt(str);
+}
+
+function toStr(int) {
+  return `${int}`;
+}
+
+function toInt(str) {
+  return parseInt(str);
+}
+
+function toStr(int) {
+  return int.toString();
+}
+
+const toInt = (s) => +s;
+const toStr = (x) => x + '';
+
+function toInt(str) {
+  return Number(str);
+}
+
+function toStr(int) {
+  return String(int);
+}
