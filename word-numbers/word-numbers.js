8080561c5d1e25d35d7b1723717db9ecadf62017
dvlprwchoi <dvlpr.wchoi@gmail.com>
2022-07-31 10:59:46 -0500

    Add word numbers

---
+++ word-numbers/word-numbers.js
@@ -0,0 +1,55 @@
+function word(s) {
+  switch (s) {
+    case 'one':
+      return 1;
+    case 'two':
+      return 2;
+    case 'three':
+      return 3;
+    case 'four':
+      return 4;
+    case 'five':
+      return 5;
+    case 'six':
+      return 6;
+    case 'seven':
+      return 7;
+    case 'eight':
+      return 8;
+    case 'nine':
+      return 9;
+    case 'zero':
+      return 0;
+  }
+}
+
+word = (s) =>
+  [
+    'zero',
+    'one',
+    'two',
+    'three',
+    'four',
+    'five',
+    'six',
+    'seven',
+    'eight',
+    'nine',
+  ].indexOf(s);
+
+function word(s) {
+  let obj = {
+    one: 1,
+    two: 2,
+    three: 3,
+    four: 4,
+    five: 5,
+    six: 6,
+    seven: 7,
+    eight: 8,
+    nine: 9,
+    zero: 0,
+  };
+
+  return obj[s];
+}
