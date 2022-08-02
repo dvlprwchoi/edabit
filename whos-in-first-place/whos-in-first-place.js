c327162909bdf713ac9744e8d23263e573b0348f
Woo-Sik Choi <dvlpr.wchoi@gmail.com>
2022-08-02 11:20:12 -0500

    Update whos in first place

--- whos-in-first-place/whos-in-first-place.js
+++ whos-in-first-place/whos-in-first-place.js
@@ -1,7 +1,25 @@
-// function firstPlace(road) {
-//   const roadArr = [...road];
-//   if (roadArr.length === 0) {
-//     return 'No road available';
-//   } else if(roadArr)
-//   }
-// }
+function firstPlace(road) {
+  const roadArr = [...road];
+  const uniqueValueArr = [...new Set(road)];
+  if (roadArr.length === 0) {
+    return 'No road available';
+  } else if (uniqueValueArr == '=') {
+    return 'No car available';
+  } else {
+    for (let i = roadArr.length - 1; i >= 0; i--) {
+      if (roadArr[i] !== '=') {
+        return roadArr[i];
+      }
+    }
+  }
+}
+
+const firstPlace = (road) => {
+  const r = [...road];
+  if (!r.length) {
+    return 'No road available';
+  }
+  return r.reverse().find((a) => a != '=') || 'No car available';
+};
+
+console.log(firstPlace('====='));
